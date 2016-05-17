bfa7ee6777e3432122bb3420c6906e8754ab36e4
bertrand <bertrand@viravong.fr>
2016-05-17 14:20:06 +0200

    fist commit

--- application/config/database.php
+++ application/config/database.php
@@ -48,9 +48,9 @@
 $active_group = 'default';
 $active_record = TRUE;
 
-$db['default']['hostname'] = 'mysql51-49.bdb';
-$db['default']['username'] = 'genessbdd';
-$db['default']['password'] = 'lR9hmOYW';
+$db['default']['hostname'] = 'localhost';
+$db['default']['username'] = 'root';
+$db['default']['password'] = '';
 $db['default']['database'] = 'genessbdd';
 $db['default']['dbdriver'] = 'mysql';
 $db['default']['dbprefix'] = '';
